00037915fb2740b74b51242d7175ebd2f30fd2eb
JPS-ONE <jpardosempere@outlook.es>
2025-01-02 23:55:47 +0100

    change vite config for custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/portfolio/'  : '/',
+  base: '/',
   build: {
     rollupOptions: {
       input: {
